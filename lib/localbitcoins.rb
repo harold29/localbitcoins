98c414e6fc253f183ba389819029e209a95c7b02
Harold Perez <haroldjose2903@gmail.com>
2019-10-22 00:08:59 -0300

    feat(localbitcoins.rb): add class elements and variables. Close #8

--- lib/localbitcoins.rb
+++ lib/localbitcoins.rb
@@ -1,6 +1,31 @@
 require "localbitcoins/version"
 
 module Localbitcoins
+  
+  @app_info = nil
+
+  @api_base = 'https://localbitcoins.com'
+
+  class << self
+    attr_accessor :apiauth_key
+    attr_accessor :apiauth_nonce
+    attr_accessor :apiauth_signature
+
+    attr_reader :api_base
+
+    def self.app_info
+      @app_info
+    end
+
+    def self.app_info=(info)
+      @app_info = info
+    end
+
+    def self.api_base=(api_base)
+      @api_base = api_base
+    end
+  end
+
   class Error < StandardError; end
   # Your code goes here...
 end
