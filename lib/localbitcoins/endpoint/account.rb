95b95f9eeb3dd56d6f3862a1b15c28da28a4656c
Harold Perez <haroldjose2903@gmail.com>
2019-11-06 08:06:15 -0300

    chore(account): add account module with first functionalities

---
+++ lib/localbitcoins/endpoint/account.rb
@@ -0,0 +1,29 @@
+module Localbitcooins
+  module Endpoint
+    module Account
+      def myself(options={})
+        get('myself/', options)
+      end
+
+      def dashboard(options={})
+        get('dashboard/', options)
+      end
+
+      def dashboard_released(options={})
+        get('dashboard/released/', options)
+      end
+
+      def dashboard_canceled(options={})
+        get('dashboard/canceled/', options)
+      end
+
+      def dashboard_closed(options={})
+        get('dashboard/closed/', options)
+      end
+
+      def notifications(options={})
+        get('notifications/', options)
+      end
+    end
+  end  
+end
