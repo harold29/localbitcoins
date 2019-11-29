ba10177238c84016adf60678286b1b104d8e1ebb
Harold Perez <haroldjose2903@gmail.com>
2019-11-28 22:55:25 -0300

    Add invalid client to helpers file

--- spec/helpers.rb
+++ spec/helpers.rb
@@ -10,6 +10,11 @@ module Helpers
     @valid_client ||= Localbitcoins::Client.new(hmac_key: @hmac_key, hmac_secret: @hmac_secret)
   end
 
+  def invalid_client
+    @invalid_client ||= Localbitcoins::Client.new(hmac_key: 'hmac-invalid-key', 
+                                                  hmac_secret: 'hmac-invalid-secret')
+  end
+
   def valid_response
     include({ 'ok' => true })
   end
