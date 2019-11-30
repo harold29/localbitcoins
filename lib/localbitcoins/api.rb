d4a2b0eb36e252058c63a4cf8cdbd99d05c3467d
Harold Perez <haroldjose2903@gmail.com>
2019-11-30 20:09:00 -0300

    test: Add tests and mocks for localbitcoins client. First passing tests. Configuration, vcr cassettes were also added.

--- lib/localbitcoins/api.rb
+++ lib/localbitcoins/api.rb
@@ -1,5 +1,6 @@
 require './lib/localbitcoins/connection'
 require './lib/localbitcoins/configuration'
+require './lib/localbitcoins/error'
 require './lib/localbitcoins/request'
 require './lib/localbitcoins/endpoint'
 
@@ -8,6 +9,8 @@ module Localbitcoins
     attr_accessor(*Configuration::VALID_OPTIONS_KEYS)
 
     def initialize(options = {})
+      raise Localbitcoins::MissingKey if options[:hmac_key].nil? || options[:hmac_secret].nil?
+
       options = Localbitcoins.options.merge(options)
       Configuration::VALID_OPTIONS_KEYS.each do |key|
         send("#{key}=", options[key])
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,10 +1,113 @@
 require_relative './spec_helper'
 
 RSpec.describe Localbitcoins::Client, :vcr do
+  describe :initialize do
+    let(:subject) { Localbitcoins::Client }
+    
+    context 'missing keys' do
+      it 'should return a error message with missing hmac key' do
+        expect{ subject.new(hmac_secret: 'test-secret') }.to raise_error(Localbitcoins::MissingKey)
+      end
+
+     it 'should return an error message with a missing hmac secret' do
+       expect{ subject.new(hmac_key: 'test-key') }.to raise_error(Localbitcoins::MissingKey)
+     end
+    end
+    
+    context 'with hmac keys' do
+      it 'should return an intialized client' do
+        expect(subject.new(hmac_key: 'test-key', hmac_secret: 'test-secret').class).to eq(Localbitcoins::Client)
+      end
+    end
+  end
+
   describe :account do
     describe :myself do
-      it 'gets user data with a valid token' do
-        # expect(valid_client.myself).to valid_response
+      context 'valid token' do
+        it 'should return username and user url' do
+          response = valid_client.myself
+          body = JSON.parse(response.body, symbolize_names: true)[:data]
+          expect(response.status).to eq(200)
+          expect(body[:username]).to eq('test-user')
+          expect(body[:url]).to eq('https://localbitcoins.com/p/test-user/')
+        end
+      end
+
+      context 'invalid token' do
+        it 'should return an error code and message' do
+          response = invalid_client.myself
+          error_body = JSON.parse(response.body, symbolize_names: true)[:error]
+          expect(response.status).to eq(400)
+          expect(error_body[:error_code]).to eq(41)
+          expect(error_body[:message]).to eq('HMAC authentication key and signature was given, but they are invalid.')
+        end
+      end
+    end
+
+    describe '#dashboard' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+
+    describe '#dashboard_released' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+
+    describe '#dashboard_closed' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+
+    describe '#notifications' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+  end
+
+  describe :wallet do
+    describe '#wallet' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+    describe '#wallet_balance' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+    describe '#wallet_address' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+    describe '#fees' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+    describe '#wallet_send' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
+      end
+    end
+    describe '#wallet_send_pin' do
+      it 'is' do
+        pending('waiting to be implemented')
+        expect(1).to be(1)
       end
     end
   end
